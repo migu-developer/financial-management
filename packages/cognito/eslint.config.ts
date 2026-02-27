164a3fd17694cc660e37bb99b4ef1677904abe33
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2026-02-27 00:21:40 -0500

    chore(infra): adding support for email builder

--- packages/cognito/eslint.config.ts
+++ packages/cognito/eslint.config.ts
@@ -1,3 +1,11 @@
 import { nodeConfig } from '@packages/config/eslint';
 
-export default nodeConfig;
+export default [
+  { ignores: ['node_modules/'] },
+  ...(Array.isArray(nodeConfig) ? nodeConfig : [nodeConfig]),
+  {
+    rules: {
+      'no-new': 'off',
+    },
+  },
+];
