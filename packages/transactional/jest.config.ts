d542ad6f0a905378fc6c7f77ff990521aaac9d88
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2026-03-03 00:51:56 -0500

    ci(project): Adding changes on test files for CI checks

--- packages/transactional/jest.config.ts
+++ packages/transactional/jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/jest.env.ts'],
   roots: [
     '<rootDir>/components',
     '<rootDir>/config',
