f10829e4ec34c017cf3f305eee4a5cea131cd526
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2026-04-05 22:44:32 -0500

    feat(client): Adding support for creating deleting and updating expenses

--- client/main/tests/app/dashboard/expenses/index.test.ts
+++ client/main/tests/app/dashboard/expenses/index.test.ts
@@ -1,5 +1,8 @@
 import DashboardExpensesScreen from '@/app/dashboard/expenses/index';
 
+process.env.EXPO_PUBLIC_API_URL =
+  'https://test.execute-api.us-east-1.amazonaws.com/dev';
+
 describe('app/dashboard/expenses/index', () => {
   it('exports a default function', () => {
     expect(typeof DashboardExpensesScreen).toBe('function');
